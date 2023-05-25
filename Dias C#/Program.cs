05b5752f57e106c87acea29a1db3a5cddd8a313d
diobi <diobi@LAPTOP-DIAS>
2023-05-25 17:17:05 +0500

    Добавьте файлы проекта.

---
+++ Dias C#/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Выберите +,-,*,/");
+String a = Console.ReadLine();
+
+Console.WriteLine("Введите 1 число");
+int A;
+A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите 2 число ");
+int B; 
+B = int.Parse(Console.ReadLine());
+switch (a)
+{
+    case "+":
+        Console.WriteLine("Ок,Result:" + (A + B));
+        break;
+
+    case "-":
+        Console.WriteLine("Ok,Result:  " + (A - B));
+        break;
+
+    case "*":
+        Console.WriteLine("OK,Result:" + (A * B));
+
+        break;
+
+    case "/":
+        Console.WriteLine("OK,Result:" + (A / B));
+        break;
+}
